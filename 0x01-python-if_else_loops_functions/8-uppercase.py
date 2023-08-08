58ef402df6d87bd01e06a91ffc240b9c6622a5f7
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-08 08:30:29 +0300

    saikoo commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    print(f"""{
+        ''.join(chr(ord(i)-(ord('a')-ord('A')))
+        if ord(i) - ord('a') >= 0 else i for i in str)
+            }""")
+
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
