7b461969eab87ae3be35d5a0469b32f597080676
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-08 12:21:52 +0300

    saikoo commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if (i % 3 == 0 and i % 5 == 0):
-            print("FIZZBUZZ", end=' ')
+            print("FizzBuzz", end=' ')
         if (i % 3 == 0):
-            print("FIZZ", end=' ')
+            print("Fizz", end=' ')
         elif (i % 5 == 0):
-            print("BUZZ", end=' ')
+            print("Buzz", end=' ')
         else:
             print(i, end=' ')
