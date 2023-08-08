829892cdd9c6b56d24c72c142ce4cc304dab0c7e
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-08 12:20:36 +0300

    saikoo commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print("""{}"""
-          .format(''.join(chr(ord(i)-(ord('a')-ord('A')))
-        if ord(i) - ord('a') >= 0 else i for i in str)))
+    print("""{}""".format(''.join(chr(
+        ord(i)-(ord('a')-ord('A'))
+        )if ord(i) - ord('a') >= 0 else i for i in str)))
