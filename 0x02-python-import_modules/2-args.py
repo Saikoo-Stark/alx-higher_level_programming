fab387b66f6a90f7da1d5cc5b48bd11871b6a179
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-10 18:09:38 +0300

    saikoo commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,8 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv
     args = len(argv)-1
-    print("{} argument{}{}".format(args, 's' if args > 1 or args == 0 else '', ':' if args > 0 else '.'))
+    a = 's' if args > 1 or args == 0 else ''
+    b = ':' if args > 0 else '.'
+    print("{} argument{}{}".format(args, a, b))
     for a, b in enumerate(argv[1:]):
         print("{}: {}".format(a+1, b))
