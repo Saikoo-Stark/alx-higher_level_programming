34e4a9a0bc1440d6ad4f79566188ed6f662405f6
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-12 15:01:46 +0300

    saikoo commit

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,11 @@
 #include "listobject.h"
 #include "object.h"
+
+/**
+ * print_python_list_info - frees a listint_t list
+ * @p: pointer to list to be freed
+ * Return: void
+ */
 void print_python_list_info(PyObject *p)
 {
 	int i, l;
