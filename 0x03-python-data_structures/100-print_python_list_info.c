ae379afcf7e4c3d74ae59dd588f5b3ba31a81aa8
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-12 15:04:10 +0300

    saikoo commit

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,6 @@
-#include "listobject.h"
-#include "object.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
 
 /**
  * print_python_list_info - frees a listint_t list
@@ -8,9 +9,10 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	int i, l;
+	int i;
 
-	l = PyList_Size(p);
-	for (i = 0; i < l; i++)
-		printf("[*] Size of the Python List = %d", l);
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < Py_SIZE(p); i++)
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 }
