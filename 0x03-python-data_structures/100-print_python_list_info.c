0adbb63d615be0ad7fb509e227ca4b9cc985ef6e
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-12 15:02:52 +0300

    saikoo commit

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,6 +10,7 @@ void print_python_list_info(PyObject *p)
 {
 	int i, l;
 
-	l = PyList_Size(p) for (i = 0; i < l; i++)
+	l = PyList_Size(p);
+	for (i = 0; i < l; i++)
 		printf("[*] Size of the Python List = %d", l);
 }
