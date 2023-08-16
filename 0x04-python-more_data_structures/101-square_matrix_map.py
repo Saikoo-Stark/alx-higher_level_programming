d5bd695cd26039db6ff55f763db09dfb9841349f
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-16 21:15:39 +0300

    saikoo commit

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map(lambda inner: list(map(lambda key: key*key,
-                                           inner)), matrix))
+def square_matrix_map(m=[]):
+    return list(map(lambda i: list(map(lambda key: key*key, i)), m))
