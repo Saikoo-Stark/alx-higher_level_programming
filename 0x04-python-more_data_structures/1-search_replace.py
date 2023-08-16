9879dca3d849fdc62404e58290af25a0abda5903
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-16 19:17:15 +0300

    saikoo commit

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    li = my_list.copy()
+    for i in range(0, len(li)):
+        if li[i] == search:
+            li[i] = replace
+    return li
