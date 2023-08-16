84ca93f72182cf1f5a0da6553099c58f7af77621
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-16 20:49:54 +0300

    saikoo commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary.keys():
-        a_dictionary[i] *= 2
-    return a_dictionary
+    dica = a_dictionary.copy()
+    for i in dica.keys():
+        dica[i] = dica[i]*2
+    return dica
