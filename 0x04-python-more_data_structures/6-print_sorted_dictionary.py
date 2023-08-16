d919323b8c232ad774fed1dfda87ebd7687ed00d
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-16 19:36:21 +0300

    saikoo commit

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    dic = dict(sorted(a_dictionary.items(), key=lambda item: item[0].lower()))
+    for i, j in dic.items():
+        print(f"{i}: {j}")
