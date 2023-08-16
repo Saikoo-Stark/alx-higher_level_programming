4143ca405b99a734d433a5e000051e5a3d982837
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-16 19:39:01 +0300

    saikoo commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    dic = dict(sorted(a_dictionary.items(), key=lambda item: item[0])
+    dic = dict(sorted(a_dictionary.items()))
     for i, j in dic.items():
         print(f"{i}: {j}")
