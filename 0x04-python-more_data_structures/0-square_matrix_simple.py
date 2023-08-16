40f7637b11d29caf0c0db712c5866dfd3a0df8be
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-16 21:16:27 +0300

    saikoo commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(mat=[]):
-    matrix = mat.copy()
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            matrix[i][j] = matrix[i][j]**2
-    return matrix
+def square_matrix_map(m=[]):
+    return list(map(lambda i: list(map(lambda key: key*key, i)), m))
