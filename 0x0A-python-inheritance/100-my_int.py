78d5ba0699de0d6d150c6a5d0a957fe0c4db6815
Saikoo-Stark <alwkylsyd87@gmail.com>
2024-02-06 03:31:36 +0200

    saikoo

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class MyInt(int):
+    
+    def __int__(self, value: int):
+        super().__int__(value)
+        
+    def __eq__(self, __value: int) -> bool:
+        return not super().__eq__(__value)
+    
+    def __ne__(self, __value: object) -> bool:
+        return not super().__ne__(__value)
