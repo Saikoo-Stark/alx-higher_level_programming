9bae9b5a9945295579e517b50e54ce2e0a621ff7
Saikoo-Stark <alwkylsyd87@gmail.com>
2024-02-06 12:00:04 +0200

    uploaded

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""
-adds all arguments to a Python list
-"""
 import sys
+"""access commandline arguments"""
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""create object from JSON file"""
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+"""writes an object to text file, using JSON representation"""
 
 
 save = __import__('5-save_to_json_file').save_to_json_file
