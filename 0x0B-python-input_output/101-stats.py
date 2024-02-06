09addc5e516f18bb6ebb1031566b21fc93b2c78f
Saikoo-Stark <alwkylsyd87@gmail.com>
2024-02-06 14:35:04 +0200

    uploaded

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
-"""
-define append module
-"""
-
 import sys
 
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-                "404": 0, "405": 0, "500": 0}
-total_size = 0
-r = 0
-for line in sys.stdin:
-    status, size = line.split()[7], line.split()[8]
-    r += 1
-    if status not in status_codes:
-        continue
-    total_size += int(size)
-    status_codes[status] += 1
-    if r == 10:
-        print("File size: {}".format(total_size))
-        for i, j in status_codes.items():
-            if j != 0:
-                print("{}: {}".format(i, j))
-        r = 0
+def print_status():
+    '''
+        Printing the status of the request
+    '''
+    counter = 0
+    size = 0
+    file_size = 0
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
+
+    for l in sys.stdin:
+        line = l.split()
+        try:
+            size += int(line[-1])
+            code = line[-2]
+            status_codes[code] += 1
+        except:
+            continue
+        if counter == 9:
+            print("File size: {}".format(size))
+            for key, val in sorted(status_codes.items()):
+                if (val != 0):
+                    print("{}: {}".format(key, val))
+            counter = 0
+        counter += 1
+    if counter < 9:
+        print("File size: {}".format(size))
+        for key, val in sorted(status_codes.items()):
+            if (val != 0):
+                print("{}: {}".format(key, val))
+
+
+if __name__ == "__main__":
+    print_status()
