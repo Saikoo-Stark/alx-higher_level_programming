e1faa56153662fc56792ef76a817ca9008f47975
Saikoo-Stark <alwkylsyd87@gmail.com>
2024-02-06 14:34:19 +0200

    uploaded

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,21 +6,20 @@ define append module
 import sys
 
 
-status_op = ['200', '301', '400', '401', '403', '404', '405', '500']
-
-
-statuses = {}
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+                "404": 0, "405": 0, "500": 0}
 total_size = 0
 r = 0
 for line in sys.stdin:
     status, size = line.split()[7], line.split()[8]
     r += 1
-    if status not in status_op:
+    if status not in status_codes:
         continue
     total_size += int(size)
-    statuses[status] = statuses.get(status, 0) + 1
+    status_codes[status] += 1
     if r == 10:
         print("File size: {}".format(total_size))
-        for i, j in statuses.items():
-            print("{}: {}".format(i, j))
+        for i, j in status_codes.items():
+            if j != 0:
+                print("{}: {}".format(i, j))
         r = 0
