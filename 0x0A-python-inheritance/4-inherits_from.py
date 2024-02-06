cf6528a9937e35018cd51393680c4fcca8485e93
Saikoo-Stark <alwkylsyd87@gmail.com>
2024-02-06 15:02:01 +0200

    uploaded

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defining class Mylist
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Defining of function
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
