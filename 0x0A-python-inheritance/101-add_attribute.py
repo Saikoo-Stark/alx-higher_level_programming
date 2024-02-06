9b920e6059c95de8f61bea5f6714b386cb64d928
saikoostark <alwkylsyd87@gmail.com>
2024-02-06 04:33:08 +0200

    update

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,5 +2,9 @@
 """Defines a module 101-add_attribute"""
 
 
-def add_attribute(obj : object, attr_name : str , attr_value : object):
+def add_attribute(obj: object, attr_name: str , attr_value: object):
+    """
+    add_attribute function
+    """
+
     obj.__setattr__(attr_name, attr_value)
