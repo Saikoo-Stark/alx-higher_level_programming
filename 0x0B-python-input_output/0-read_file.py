251c300cf22ac13e52bf37f6899dd56363b2344c
Saikoo-Stark <alwkylsyd87@gmail.com>
2024-02-06 11:36:39 +0200

    uploaded

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,8 @@ Defining class Mylist
 
 
 def read_file(filename=""):
-    """ just a function"""
-    with open(file=filename, mode="w", encoding="utf-8") as f:
+    """
+    just a function
+    """
+    with open(file=filename, mode="r", encoding="utf-8") as f:
         print(f.read())
