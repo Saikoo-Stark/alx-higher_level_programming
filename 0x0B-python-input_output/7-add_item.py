30225c17f6a992123d6ddfa9e66701fdee5f29c3
Saikoo-Stark <alwkylsyd87@gmail.com>
2024-02-06 12:01:06 +0200

    uploaded

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 """access commandline arguments"""
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 """create object from JSON file"""
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 """writes an object to text file, using JSON representation"""
 
+filename = "add_item.json"
+try:
+    content = load_from_json_file(filename)
+except Exception:
+    content = []
 
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
-
-with open("add_item.json", "a") as _:
-    try:
-        i = load("add_item.json")
-    except ValueError:
-        i = []
-    save(i + sys.argv[1:], "add_item.json")
+for i in range(1, len(argv)):
+    content.append(argv[i])
+save_to_json_file(content, filename)
