dbf2c413052f2903d1dca9896f3a3d5bc495191c
Saikoo-Stark <alwkylsyd87@gmail.com>
2024-02-06 11:33:02 +0200

    uploaded

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defining class Mylist
+"""
+
+
+def read_file(filename=""):
+    """ just a function"""
+
+    with open(file=filename, mode="w", encoding="utf-8") as f:
+        print(f.read())
